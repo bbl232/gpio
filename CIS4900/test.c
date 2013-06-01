40647441125276c4f6118957cf8c07c98c0a13d8
Billy <bill@vandenberk.me>
2013-05-31 23:16:14 -0400

    updated test

--- CIS4900/test.c
+++ CIS4900/test.c
@@ -5,7 +5,17 @@ int main(){
 	printPinMap();
 	PIN mypin = On(1);
 	if(NULL == mypin) printf("Couldn't Create Pin\n");
-	printf("Pin value %s\n", (Read(mypin) ? "TRUE" : "FALSE"));
+	printf("Pin value ");
+	int read = Read(mypin));
+	if(read > 0){
+ 		printf("TRUE\n");
+ 	}
+ 	else if(read < 0){
+ 		printf("FALSE\n");
+ 	}
+ 	else{
+ 		printf("UNDEF\n");
+ 	}
 	printf("Set value false: %s\n", (Write(mypin,false) ? "TRUE" : "FALSE"));
 	printf("Set value true: %s\n", (Write(mypin,true) ? "TRUE" : "FALSE"));
 	printf("Pin off: %s\n", (Off(mypin) ? "TRUE" : "FALSE"));
