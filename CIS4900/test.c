a61e9244ca3f915edfd03269512fcb9789d3c569
Bill Vandenberk <bill@vandenberk.me>
2013-07-24 12:05:32 +0000

    Changed test file

--- CIS4900/test.c
+++ CIS4900/test.c
@@ -6,13 +6,30 @@ int main(){
 	int relays[4] = {0,1,2,3};
 	int i = 0;
 
-	for(i=0;i<4;i++){
-		setValue(relays[i],true);
-	}
-	for(i=0;i<4;i++){
-		setValue(relays[i],false);
-	}
-	for(i=0;i<4;i++){
-		setValue(relays[i],true);
-	}
+	PIN ap1 = PinOn(0);
+	PIN ap2 = PinOn(1);
+	PIN ap3 = PinOn(2);
+	PIN ap4 = PinOn(3);
+	SetLogic(ap1,ACTIVE_LOW);
+	SetLogic(ap2,ACTIVE_LOW);
+	SetLogic(ap3,ACTIVE_LOW);
+	SetLogic(ap4,ACTIVE_LOW);
+	SetDirection(ap1,OUT);
+	SetDirection(ap2,OUT);
+	SetDirection(ap3,OUT);
+	SetDirection(ap4,OUT);
+	Write(ap1,true);
+	Write(ap2,true);
+	Write(ap3,true);
+	Write(ap4,true);
+	sleep(1);
+	Write(ap1,false);
+	Write(ap2,false);
+	Write(ap3,false);
+	Write(ap4,false);
+	PinOff(ap1);
+	PinOff(ap2);
+	PinOff(ap3);
+	PinOff(ap4);
+
 }
